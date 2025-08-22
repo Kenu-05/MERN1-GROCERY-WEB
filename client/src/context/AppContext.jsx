67fab8356228aba3ecd50d6db6d85ff1141b5655
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-22 14:01:15 +0530

    Initial commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,69 @@
+import { createContext,useContext,useEffect,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children})=>{
+
+const currency=import.meta.VITE_CURRENCY;
+
+const navigate = useNavigate();
+const [user, setUser] = useState(true)
+const [isSeller, setIsSeller] = useState(false)
+const [showUserLogin, setShowUserLogin] = useState(false)
+const [products, setProducts] = useState([])
+const [cartItems,setCartItems]=useState({})
+
+
+//fetch all products
+const fetchProducts=async ()=>{
+  setProducts(dummyProducts)
+}
+
+//Add product to cart
+const addToCart=()=>{
+  let cartData=structureClone(cartItems);
+  if (cartData[itemId]){
+    cartData[itemId]+=1;
+  }else{
+    cartData[itemId]=1;
+  }
+  setCartItems(cartData);
+  toast.success("Added to Cart") /*in App.jsx under the nav bar need to add toast component*/
+  
+}
+//update cart Item quantity
+const updateCartItem=(itemId,quantity)=>{
+  let cartData=structureClone(cartItems);
+  cartData[itemId]=quantity;
+  setCartItems(cartData)
+  toast.success("cart Updated")
+}
+//Remove from cart
+const removeFromCart = (itemId)=>{
+   let cartData = structuredClone(cartItems);
+   if(cartData[itemId]){
+      cartData[itemId] -= 1;
+       if(cartData[itemId] === 0){
+          delete cartData[itemId];
+       }
+
+toast. success("Removed from Cart")
+setCartItems(cartData)
+}
+}
+
+useEffect(()=>{fetchProducts()},[])
+
+const value = {navigate, user, setUser, setIsSeller, isSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems}
+
+  return <AppContext.Provider value={value}>
+       {children}
+   </AppContext.Provider>
+}
+
+export const useAppContext = ()=>{
+return useContext(AppContext)
+
+}
