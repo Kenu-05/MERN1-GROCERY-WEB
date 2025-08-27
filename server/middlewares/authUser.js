709872f88d9aaa803f0e08449df7d89d3d4deb74
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-27 12:52:22 +0530

    Created Middleware checking authUser in auth.js

---
+++ server/middlewares/authUser.js
@@ -0,0 +1,21 @@
+const authUser = async (req, res, next)=>{
+const {token} = req.cookies;
+
+      if(!token){
+        return res.json({ success: false, message: 'Not Authorized' });
+      }
+
+      try {
+         const tokenDecode = jwt. verify(token, process. env. JWT_SECRET)
+         if(tokenDecode.id){
+             req.body.userId = tokenDecode.id;
+         }else{
+           return res.json({ success: false, message: 'Not Authorized' });
+         }I
+       next (); // exicute the controller
+       } 
+      catch (error) {
+         res.json({success:false,message:error.message})
+}
+}
+export default authUser;
