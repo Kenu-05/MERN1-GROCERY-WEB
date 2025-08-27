04c52b1960399a8bb56ce2212a8182cbe38f18af
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-27 13:50:27 +0530

    Logout Function in user Controller is Done

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -87,4 +87,19 @@ export const register = async (req, res)=>{
             }
 
 }
-    
+// Logout User : /api/user/logout
+
+  export const logout = async (req, res)=>{
+        try {
+           res. clearCookie('token', {
+           httpOnly: true,
+           secure: process. env. NODE_ENV === 'production',
+           sameSite: process. env.NODE_ENV === 'production' ? 'none' : 'strict',
+           });
+
+           return res. json({ success: true, message: "Logged Out" })
+           } catch (error) {
+            console.log(error.message);
+                res. json({ success: false, message: error.message });
+             }
+}
