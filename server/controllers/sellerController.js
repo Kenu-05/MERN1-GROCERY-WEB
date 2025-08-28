d2c15d79998fe84a57c57661b1fac570d623e649
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-28 12:20:10 +0530

    creating sellerLogout function and create routes for that

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -26,3 +26,36 @@ export const sellerLogin = async (req, res) =>{
             res.json({success:false,message:error.message});
          }
 }
+
+ //check Seller auth:/api/seller/is-auth
+        export const isSellerAuth = async (req, res)=>{
+             
+           try {
+             
+              return res.json({success:true})
+              
+            } 
+            catch (error) {
+                console.log(error.error);
+                res.json({ success: false, message: error.message });
+            }
+
+}
+// Logout Seller : /api/seller/logout
+
+  export const sellerLogout = async (req, res)=>{
+        try {
+           res.clearCookie('SellerToken',{
+           httpOnly: true,
+           secure: process.env.NODE_ENV === 'production', 
+           sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'strict',
+           maxAge: 7 * 24 * 60 * 60 * 1000,
+
+           });
+
+           return res.json({ success: true, message: "Logged Out" })
+           } catch (error) {
+            console.log(error.message);
+                res.json({ success: false, message: error.message });
+             }
+}
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 
 
@@ -23,6 +24,7 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res) => res.send("API is Working"));
 app.use('/api/user',userRouter);
+app.use('/api/seller',sellerRouter);
 
 
 app.listen(port, ()=>{
