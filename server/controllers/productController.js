2c741e6ddcd1e52551924ea6aac9ca4891014c58
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-28 13:26:34 +0530

    Creating Product controller file ,Product Model Created,Cloudinary Config File created

---
+++ server/controllers/productController.js
@@ -0,0 +1,19 @@
+
+
+
+//Add Product:/api/product/add
+export const addProduct=async(req,res)=>{
+
+}
+//Get Product:/api/product/get
+export const getProduct=async(req,res)=>{
+
+}
+//get single Product:/api/product/id
+export const productById=async(req,res)=>{
+
+}
+//Change Product inStock:/api/product/stock
+export const changeStock=async(req,res)=>{
+
+}
