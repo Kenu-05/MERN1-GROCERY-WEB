708be07b9a43994d3929b4fe287a289d32410899
Kenuja Sivakumar <sivakum-se21050@stu.kln.ac.lk>
2025-08-31 22:37:07 +0530

    Done fetchAllOrders in seller page with backend

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -5,12 +5,22 @@ import { useEffect,useState } from "react";
 
 const Orders = () => {
 
-        const {currency} = useAppContext ();
+        const {currency,axios} = useAppContext ();
         const [orders, setOrders] = useState([])
 
-        const fetchOrders = async () =>{
-           setOrders (dummyOrders)
+        const fetchOrders = async () =>{         
+              try {
+                const { data } = await axios.get('/api/order/seller');
+                if(data.success){
+                  setOrders(data.orders)
+                }else{
+                   toast.error(data.message)
+             } 
+            }catch (error) {
+                toast.error(error.message)
+           
         };
+    }
 
         useEffect(()=>{
              fetchOrders();
